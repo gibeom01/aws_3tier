6d5cc6e78618418abf24871a2fbb9457a9797df6
gibeom01 <rlqja7638@gmail.com>
2024-11-10 16:46:12 +0900

    Add files via upload

---
+++ docker-install.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+## INFO: https://docs.docker.com/engine/install/ubuntu/
+
+set -euf -o pipefail
+
+DOCKER_USER=ubuntu  # 서버 사용자명과 동일하게 설정하세요
+
+# Install dependencies
+sudo apt-get update && sudo apt-get install -y \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg \
+  lsb-release
+
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Set up the stable repository
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker CE
+sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Start and enable Docker service
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Use Docker without root
+sudo usermod -aG docker $DOCKER_USER
+
+# Apply group changes without restarting
+newgrp docker << EOF
+echo "Docker installation completed. You can now use Docker commands without sudo."
+EOF
